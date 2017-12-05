9f1bb79a0c0cbba2ca414c010a69ccbd5b97e357
Jackie <jacgut19@gmail.com>
2017-12-05 14:19:58 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+//Compound Assignment With Augmented Addition
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+output: 
+a = 15
+b = 26
+c = 19
+
+
+//Compound Assignment With Augmented Subtraction
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+output:
+a = 5
+b = -6
+c = 2
+
+
+//Compound Assignment With Augmented Multiplication 
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+output:
+a = 25
+b = 36
+c = 46
+
+
+//Compound Assignment With Augmented Division
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+output:
+a = 4
+b = 27
+c = 3
+
